6c7f9f783a3ed4c93fe7ac2d7177ecddc25fc3c9
João Felipe <joao.si23@outlook.com>
2020-07-02 14:48:17 -0300

    Here I push exercices about Functions

---
+++ estudandoFunçoesCalculo.js
@@ -0,0 +1,13 @@
+function dizerNome(){
+    console.log ('João');
+};
+
+dizerNome();
+
+function multiplicarPorDois (valor){
+    return valor*2;
+};
+
+let resultado = multiplicarPorDois(5);
+
+console.log (resultado);
